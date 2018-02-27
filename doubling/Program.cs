c09489655d62780ebe59e822e0fd36e5041c8e71
Atis0505 <attilakorom2014@gmail.com>
2018-02-27 17:53:21 +0100

    Add project files.

---
+++ doubling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an integer variable named `ak` and assign the value `123` to it
+            int ak = 123;
+            // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+            // - Print the result of `doubling(ak)`
+            Console.WriteLine("Result of doubling: {0}",doubling(ak));
+            Console.ReadLine();
+        }
+
+        public static int doubling (int number)
+        {
+            return number * 2;
+        }
+    }
+}
